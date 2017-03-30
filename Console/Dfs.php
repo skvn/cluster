d7a184e48e00371050b6645ff47fc49d2450d014
serg <ohqncp>
2017-03-30 05:52:09 +0300

    -- sync

--- Console/Dfs.php
+++ Console/Dfs.php
@@ -63,6 +63,9 @@ class Dfs extends ConsoleActionEvent
         $this->app->cluster->heartbeat();
     }
 
+    /**
+     * Check replication state on cluster node
+     */
     function actionCheckHeartbeat()
     {
         if (!$this->app->cluster->checkHeartbeat()) {
@@ -74,6 +77,9 @@ class Dfs extends ConsoleActionEvent
         }
     }
 
+    /**
+     * Ping all nodes
+     */
     function actionPing()
     {
         $down = $this->app->cluster->pingNodes();
@@ -85,6 +91,26 @@ class Dfs extends ConsoleActionEvent
                 ]));
             }
         }
+    }
+
+
+    /**
+     * Sync by rsync files in one section
+     * @option number *section Number of section to sync
+     * @option number *target Target host to sync
+     */
+    function actionSyncSection()
+    {
+        $this->app->db->disconnect();
+        $command = $this->app->cluster->getOption('rsync_command');
+        $command .= $this->app->getPath($this->app->cluster->getOption("sections_path")) . "/" . $this->options['section'] . "/ ";
+        $host = $this->app->cluster->getHostById($this->options['target']);
+        $command .= $host['img'] . "::section" . $this->options['section'];
+        exec($command, $result);
+        $this->stdout("Section " . $this->options['section'] . " copied to node " . $this->options['target']);
+        $this->stdout("Result: ");
+        $this->stdout($result);
+
     }
 
 
